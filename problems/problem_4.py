be7ac9b4bbd8f280dd0159f3b4557d9471cef229
divanvanrooyen <divan@roonieschmidt.com>
2025-12-08 22:39:18 +0200

    Problem 4

---
+++ problems/problem_4.py
@@ -0,0 +1,35 @@
+first_half = []
+last_half = []
+
+def is_palindrome(num):
+    str_num = str(num)
+    i = 0
+    n = len(str_num)-1
+    while i < len(str_num)/2:
+        first_half.append(str_num[i])
+        i += 1
+    
+    while n >= len(str_num)/2:
+        last_half.append(str_num[n])
+        n -= 1
+    
+    if first_half == last_half:
+        print("Is Palindrome")
+        return True
+    else:
+        return False
+
+# is_palindrome(906609)
+
+# 3 Digits, largest Palindrome
+largest_palindrome = 0
+
+for x in range(999, 99, -1):
+    for y in range(999, 99, -1):
+        product = x * y
+        if str(product) == str(product)[::-1]: # Test for palindrome
+            if product > largest_palindrome:
+                largest_palindrome = product
+    
+print(largest_palindrome)
+
